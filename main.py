d48dca6673f67bb66aef7bff0b07bda2cfdceeb7
Sifat-16 <sifatalamcep16@gmail.com>
2024-06-10 13:33:19 +0600

    update asset link

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -12,11 +14,18 @@ app.add_middleware(
 )
 
 
-app.mount("/.well-known", StaticFiles(directory="static/.well-known"), name="well-known")
+#app.mount("/.well-known", StaticFiles(directory="static/.well-known"), name="well-known")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# Serve the assetlinks.json file directly without redirects
+@app.get("/.well-known/assetlinks.json", response_class=FileResponse)
+def read_assetlinks():
+    json_path = os.path.join("static", ".well-known", "assetlinks.json")
+    return FileResponse(path=json_path, media_type="application/json")
+
 
 @app.get("/community")
 def read_root():
